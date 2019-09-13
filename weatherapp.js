df0de14baaf238aef5d0a91ef2753ffa9af6b817
nsl10 <42556882+nsl10@users.noreply.github.com>
2019-09-13 02:14:31 -0400

    Update weatherapp.js

--- weatherapp.js
+++ weatherapp.js
@@ -12,7 +12,7 @@ window.addEventListener("load", ()=> {
 		
 		return fetch(https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/7a932a5a33acf352e36e63f87af62440/37.8267,-122.4233)
 			.then(res => res.json())
-			.then(post =>console.log(posts))
+			.then(post =>console.log(posts));
 		});
 	}
 
