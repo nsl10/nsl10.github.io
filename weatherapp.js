c19b0c34986c7590b7e96bff6d9232d993724bab
nsl10 <42556882+nsl10@users.noreply.github.com>
2019-09-21 10:39:06 -0700

    Update weatherapp.js

--- weatherapp.js
+++ weatherapp.js
@@ -10,7 +10,7 @@ window.addEventListener("load", ()=> {
 			const proxy = "https://cors-anywhere.herokuapp.com/";
 			const api = '${proxy}https://api.darksky.net/forecast/7a932a5a33acf352e36e63f87af62440/37.8267,-122.4233';
 		
-		fetch(https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/7a932a5a33acf352e36e63f87af62440/37.8267,-122.4233))
+		fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/7a932a5a33acf352e36e63f87af62440/37.8267,-122.4233')
 			.then(response => { 
 		      	return response.json();
 			})
